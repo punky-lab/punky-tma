d1260458cd849f2c142827565449427ddfd834cd
FrozenArcher <yhy242989105@hotmail.com>
2024-09-17 12:43:55 +0800

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Root } from '@/components/Root/Root';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
-import './_assets/globals.css';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Your Application Title Goes Here',
