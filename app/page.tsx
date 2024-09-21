9718ec126622b0d75c6cf366a5b9aa17b4d01c75
FrozenArcher <yhy242989105@hotmail.com>
2024-09-21 09:24:27 +0800

    add close

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import FrameAnimation from "@/components/Animation";
 import Main from "@/components/MainUI/Main";
 import Store from "@/components/MainUI/Store";
 import User from "@/components/MainUI/User";
 import { UIState } from "@/lib/UI";
 import { ReactNode, useCallback, useState } from "react";
+import punkyFrames from "@/assets/animations/punky/idle";
 
 export default function Home() {
   const [currentUI, setCurrentUI] = useState<UIState>("main");
@@ -19,6 +21,16 @@ export default function Home() {
   return (
     <div className="h-screen w-screen bg-[url('../assets/scenes/main.png')] bg-cover bg-top">
       {UIShowing.get(currentUI)}
+      {/* <div className="absolute w-screen h-screen flex flex-col items-center top-1/3 z-0">
+        <div className="-translate-y-1/3">
+          <FrameAnimation
+            frames={punkyFrames}
+            interval={1000}
+            width={180}
+            height={180}
+          />
+        </div>
+      </div> */}
     </div>
   );
 }
--- components/MainUI/PopUp.tsx
+++ components/MainUI/PopUp.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import { ReactNode } from "react";
+import CloseIcon from "@/assets/icons/close.svg"
 
 export default function PopUp({
   onClose,
@@ -8,9 +10,11 @@ export default function PopUp({
   children?: ReactNode;
 }) {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center z-30">
       <div className="bg-[url('../assets/ui/pop-up-bg.svg')] bg-cover bg-center w-4/5 h-4/5">
-        <div onClick={onClose}>close</div>
+        <div onClick={onClose} className='w-full flex flex-row justify-end px-4 py-4'>
+          <Image src={CloseIcon} alt='close' className='w-12 h-12' />
+        </div>
         {children}
       </div>
     </div>
