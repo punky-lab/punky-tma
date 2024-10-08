d8c3d35ea9a92b7324116469f7f0c9748c6d9728
NIGHT <3210103847@zju.edu.cn>
2024-10-08 18:52:34 +0800

    接入Gemini

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -4,8 +4,8 @@ const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENROUTER_API_KEY,
   defaultHeaders: {
-    "HTTP-Referer": process.env.YOUR_SITE_URL, // Optional, for including your app on openrouter.ai rankings.
-    "X-Title": process.env.YOUR_SITE_NAME, // Optional. Shows in rankings on openrouter.ai.
+    "HTTP-Referer": process.env.SITE_URL, // Optional, for including your app on openrouter.ai rankings.
+    "X-Title": process.env.SITE_NAME, // Optional. Shows in rankings on openrouter.ai.
   }
 })
 
