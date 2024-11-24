337d33038510039b16ceb7962bd3f197728cd063
YuChanGongzhu <2556553872@qq.com>
2024-11-24 18:48:10 +0800

    update :  change button layout to sector

--- components/MainUI/Action/Action.tsx
+++ components/MainUI/Action/Action.tsx
@@ -6,37 +6,53 @@ import GameIcon from "@/assets/icons/v2/game.svg";
 import { authApis } from "@/app/normalApi";
 import { useRouter } from "next/navigation";
 
-export default function Action({ fetchUserData, setIsPetting }: { fetchUserData: () => void, setIsPetting: (isPetting: boolean) => void }) {
+export default function Action({
+  fetchUserData,
+  setIsPetting,
+}: {
+  fetchUserData: () => void;
+  setIsPetting: (isPetting: boolean) => void;
+}) {
   const handlePetTouch = async () => {
     try {
-      setIsPetting(true)
+      setIsPetting(true);
       const response = await authApis.touchPet();
-      fetchUserData()
-      setIsPetting(false)
-      console.log('摸宠物成功：', response.data);
+      fetchUserData();
+      setIsPetting(false);
+      console.log("摸宠物成功：", response.data);
     } catch (error) {
-      console.error('摸宠物失败：', error);
+      console.error("摸宠物失败：", error);
     }
   };
 
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <div className="flex mb-6 w-full items-center justify-center space-x-8">
-      <div
-        className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50 cursor-pointer"
-        onClick={handlePetTouch}
-      >
-        <Image src={HandIcon} alt="Hand" className="w-10 h-10" />
+    <div className="relative h-32 w-full mb-6">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div
+          className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50 cursor-pointer"
+          onClick={handlePetTouch}
+        >
+          <Image src={HandIcon} alt="Hand" className="w-10 h-10" />
+        </div>
       </div>
 
-      <div className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50">
-        <Image src={MicroIcon} alt="Micro" className="w-10 h-10" />
+      <div className="absolute left-1/3 top-[60%] -translate-x-1/2">
+        <div className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50">
+          <Image src={MicroIcon} alt="Micro" className="w-10 h-10" />
+        </div>
       </div>
 
-      <div className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50"
-        onClick={() => { router.push('https://runner-game.punky.app') }}>
-        <Image src={GameIcon} alt="Game" className="w-10 h-10" />
+      <div className="absolute left-2/3 top-[60%] -translate-x-1/2">
+        <div
+          className="w-12 h-12 flex items-center justify-center bg-[url(../assets/icons/button-round.svg)] bg-contain bg-center z-50"
+          onClick={() => {
+            router.push("https://runner-game.punky.app");
+          }}
+        >
+          <Image src={GameIcon} alt="Game" className="w-10 h-10" />
+        </div>
       </div>
     </div>
   );
