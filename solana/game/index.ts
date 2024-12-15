abfdd94efa5f7c98aa0c4664079c3c1db5b8899e
YuChanGongzhu <2556553872@qq.com>
2024-12-16 04:10:04 +0800

    fix - wallet bug

--- solana/game/index.ts
+++ solana/game/index.ts
@@ -24,7 +24,7 @@ export function useGameProgram(): { program: Program | undefined } {
 
   const anchorProvider = new AnchorProvider(
     connection,
-    wallet,
+    wallet as any,
     AnchorProvider.defaultOptions()
   );
 
