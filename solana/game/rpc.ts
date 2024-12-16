b14f9b5cd1f93b162202b4465c8e0d99eec5c9a4
FrozenArcher <yhy242989105@hotmail.com>
2024-12-16 20:26:16 +0800

    feat: soon test net

--- solana/game/rpc.ts
+++ solana/game/rpc.ts
@@ -2,8 +2,9 @@ const RPCs = {
   SonicOrigin: "https://api.devnet.sonic.game",
   SonicFrontierV1: "",
   SoonDevNet: "https://rpc.devnet.soo.network/rpc",
+  SoonTestNet: "https://rpc.testnet.soo.network/rpc",
 };
 
 export function currentRPC() {
-  return RPCs.SoonDevNet;
+  return RPCs.SoonTestNet;
 }
